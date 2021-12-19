6a869f5b1765e14e48f9acc979314cff06ae160f
someone <96357717+ycdhleslkrlkyeft253@users.noreply.github.com>
2021-12-18 23:43:55 -0700

    null

--- laststeps/finishsetup.sh
+++ laststeps/finishsetup.sh
@@ -3,13 +3,13 @@ echo "This is the final part of setup. Only a few things left to do."
 echo "Now deleting the setup user and files."
 sleep 2
 username=`whoami`
-sudo userdel -r setup
-sudo rm -rfv /home/setup
-sudo apt autoremove
-sudo apt clean
-rm /home/$username/.config/autostart/desktemp.desktop
-rm /home/$username/.bashrc
-mv /home/$username/.bashrc.bak /home/$username/.bashrc
+sudo userdel -r setup &>/dev/null
+sudo rm -rfv /home/setup &>/dev/null
+sudo apt autoremove -y &>/dev/null
+sudo apt clean -y &>/dev/null
+rm /home/$username/.config/autostart/desktemp.desktop &>/dev/null
+rm /home/$username/.bashrc &>/dev/null
+mv /home/$username/.bashrc.bak /home/$username/.bashrc &>/dev/null
 sleep 2
 echo "Everything seems to be done. Please refer to https://github.com/ycdhleslkrlkyeft253/linusb/blob/docs/finalthings.md (control click) for further information."
 echo "Thanks for buying and making use of this product."

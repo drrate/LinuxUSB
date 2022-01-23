bc266f0adece3ee8a02867c13507b638cd826b33
human2524 <ycdhleslkrlkyeft@hotmail.com>
2022-01-22 21:50:33 -0700

    wait it didn't add main.cpp did it?

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "curl/curl.h"
+
+int main() 
+{
+  
+  std::cout << "Welcome to the setup program for LinuxUSB. Do not be alarmed, you'll be told how to do everything every step of the way, and will have a desktop soon enough.\n";
+  std::cout << "Press [ENTER] to continue... \n";
+  getchar();
+  // i spent way too long figuring out this only to realize it was the ide causing the problem.
+
+}
